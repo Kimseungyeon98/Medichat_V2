efe6144cbd25bde39728cdf148b5cdae0eab4542
김승연 <tmddus3002@gmail.com>
2025-05-22 20:50:42 +0900

    Initial Commit

--- src/main/java/ksy/medichat/main/controller/MainController.java
+++ src/main/java/ksy/medichat/main/controller/MainController.java
@@ -1,18 +1,11 @@
 package ksy.medichat.main.controller;
 
-import ksy.medichat.consulting.vo.ConsultingVO;
-import ksy.medichat.health.vo.HealthyBlogVO;
-import ksy.medichat.util.PagingUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Controller
@@ -20,11 +13,11 @@ public class MainController {
 	
 	@GetMapping("/")
 	public String init() {
-		return "redirect:/main/main";
+		return "redirect:/main";
 	}
 	
-	@GetMapping("/main/main")
+	@GetMapping("/main")
 	public String main(HttpSession session,Model model) {
-		return "main";
+		return "main/main";
 	}
 }
