ff1ce163e624af5d6565c1c966a1bc6f2f01e2cb
KimSeungYeon <tmddus3002@gmail.com>
2025-06-12 17:40:48 +0900

    [Dev] Disease Dictionary Front Develop

--- src/main/resources/static/js/disease.js
+++ src/main/resources/static/js/disease.js
@@ -1,9 +1,5 @@
-/**
- * Created by IntelliJ IDEA.
- * User: KimSeungYeon
- * Date: 2025-06-12
- * Time: 오후 4:49
- * To change this template use File | Settings | File Templates.
- */
-
-/* Add your JavaScript code here */
+$(document).ready(function() {
+    $('#d-search-icon').click(function () {
+        $('#form-disease').submit();
+    });
+});
