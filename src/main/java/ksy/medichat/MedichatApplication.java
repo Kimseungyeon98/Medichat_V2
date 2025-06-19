79e9564859efb1b82a7793c94d8954515d3fd313
KimSeungYeon <tmddus3002@gmail.com>
2025-06-19 10:37:33 +0900

    [MODIFY] Modify Config For Not Showing Spring Security Main Page

--- src/main/java/ksy/medichat/MedichatApplication.java
+++ src/main/java/ksy/medichat/MedichatApplication.java
@@ -2,9 +2,11 @@ package ksy.medichat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+// 스프링시큐리티 기본페이지 해제
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
 public class MedichatApplication {
 
