ec1a56abb89599fef9b34846015fb2007077d39a
KimSeungYeon <tmddus3002@gmail.com>
2025-06-20 11:13:35 +0900

    [TEST] Têry Spring Security

--- src/main/java/ksy/medichat/MainController.java
+++ src/main/java/ksy/medichat/MainController.java
@@ -1,16 +1,15 @@
 package ksy.medichat;
 
-import ksy.medichat.filter.Date;
+import jakarta.servlet.http.HttpSession;
 import ksy.medichat.filter.Location;
-import ksy.medichat.filter.Search;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -29,5 +28,17 @@ public class MainController {
 
 		log.info("<<initLocation>> " + inputLocation);
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/api1")
+	@ResponseBody
+	public ResponseEntity api1() {
+		return new ResponseEntity<>("api1 입니다.",HttpStatus.OK);
+	}
+
+	@GetMapping("/api2")
+	@ResponseBody
+	public ResponseEntity api2() {
+		return new ResponseEntity<>("api2 입니다.",HttpStatus.OK);
 	}
 }
--- src/main/java/ksy/medichat/config/SecurityConfig.java
+++ src/main/java/ksy/medichat/config/SecurityConfig.java
@@ -1,7 +1,48 @@
 package ksy.medichat.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig{
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/api1").hasRole("user")
+                                .requestMatchers("/api2").hasRole("admin")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true)
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user1").password("1234").roles("user").build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
