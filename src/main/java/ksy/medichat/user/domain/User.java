5780da47d9f02e3ee138f928ccfb83fd8e981e35
KimSeungYeon <tmddus3002@gmail.com>
2025-06-25 17:39:56 +0900

    [DEV] Add Variable For Role

--- src/main/java/ksy/medichat/user/domain/User.java
+++ src/main/java/ksy/medichat/user/domain/User.java
@@ -1,11 +1,7 @@
 package ksy.medichat.user.domain;
+
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name="users")
@@ -15,7 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +22,9 @@ public class User implements UserDetails {
 
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String role;
 
     /*
 
@@ -69,44 +68,4 @@ public class User implements UserDetails {
     private Date modifyDate;
 
     */
-
-
-
-
-
-    /* implements UserDetails 에 관한 오버라이드*/
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
 }
--- src/main/java/ksy/medichat/user/dto/LoginUser.java
+++ src/main/java/ksy/medichat/user/dto/LoginUser.java
@@ -1,4 +1,33 @@
 package ksy.medichat.user.dto;
 
-public class LoginUser {
+import ksy.medichat.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class LoginUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+    }
+
+    // 기타 메서드 구현
 }
