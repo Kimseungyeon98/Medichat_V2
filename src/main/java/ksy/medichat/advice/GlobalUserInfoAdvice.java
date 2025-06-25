9284ceedcbbfc4f209edf2b0ab0d201eec4188e6
KimSeungYeon <tmddus3002@gmail.com>
2025-06-25 17:42:00 +0900

    [DEV] ControllerAdvice For LoginUser Object

--- src/main/java/ksy/medichat/advice/GlobalUserInfoAdvice.java
+++ src/main/java/ksy/medichat/advice/GlobalUserInfoAdvice.java
@@ -1,4 +1,30 @@
-package ksy.medichat.config;
+package ksy.medichat.advice;
 
+import ksy.medichat.user.dto.LoginUser;
+import ksy.medichat.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalUserInfoAdvice {
+
+    private final UserService userService;
+
+    @ModelAttribute("loginUser")
+    public LoginUser addLoginUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        System.out.println("auth.toString() : " + auth.toString());
+
+        // 로그인 안 되어 있으면 null 반환
+        if (auth == null || !auth.isAuthenticated() || auth.getPrincipal().equals("anonymousUser")) {
+            return null;
+        }
+
+        return (LoginUser)auth.getPrincipal(); // → DTO로 변환된 유저 정보
+    }
 }
