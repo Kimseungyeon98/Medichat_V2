612a8830f4e0249dba8827d526986ba2fe3be4d8
KimSeungYeon <tmddus3002@gmail.com>
2025-06-26 10:50:21 +0900

    [DEV] Add AOP For Global Log Processing

--- src/main/java/ksy/medichat/aspect/LoggingAspect.java
+++ src/main/java/ksy/medichat/aspect/LoggingAspect.java
@@ -1,4 +1,26 @@
 package ksy.medichat.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    @Before("execution(* ksy.medichat..*Controller.*(..))")
+    public void beforeControllerMethod(JoinPoint joinPoint) {
+        System.out.println("Controller: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+    }
+
+    /*@Before("execution(* ksy.medichat..*Service.*(..))")
+    public void beforeServiceMethod(JoinPoint joinPoint) {
+        System.out.println("Service: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+    }
+
+    @Before("execution(* ksy.medichat..*Repository.*(..))")
+    public void beforeRepositoryMethod(JoinPoint joinPoint) {
+        System.out.println("Repository: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+    }*/
 }
