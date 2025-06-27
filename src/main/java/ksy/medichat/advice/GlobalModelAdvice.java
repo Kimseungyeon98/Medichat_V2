07575331e5150eaeaedee46d243d7c5de1609795
KimSeungYeon <tmddus3002@gmail.com>
2025-06-27 10:36:00 +0900

    [DEV] Merge Advice Files In GlboalModelAdvice

--- src/main/java/ksy/medichat/advice/GlobalModelAdvice.java
+++ src/main/java/ksy/medichat/advice/GlobalModelAdvice.java
@@ -1,5 +1,7 @@
 package ksy.medichat.advice;
 
+import jakarta.servlet.http.HttpSession;
+import ksy.medichat.filter.Search;
 import ksy.medichat.user.dto.LoginUser;
 import ksy.medichat.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +12,25 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 @RequiredArgsConstructor
-public class GlobalUserInfoAdvice {
+public class GlobalModelAdvice {
+    
+    // 검색 조건
+    @ModelAttribute("search")
+    public Search addSearch(HttpSession session) {
+        Search search = (Search) session.getAttribute("search");
 
+        // Search 없으면 만들어서 넣어줌
+        if (search == null) {
+            search = new Search();
+        }
+        session.setAttribute("search", search);
+
+        return search;
+    }
+
+
+    // 로그인 유저
     private final UserService userService;
-
     @ModelAttribute("loginUser")
     public LoginUser addLoginUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -25,4 +42,5 @@ public class GlobalUserInfoAdvice {
 
         return (LoginUser)auth.getPrincipal(); // → DTO로 변환된 유저 정보
     }
+
 }
